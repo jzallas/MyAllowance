b33d03d94770bbc0efad761aa8ee5914ba003d53
Jonathan Zallas <jzallas@schoology.com>
2016-02-21 00:07:20 -0500

    Save and restore start date and end date

--- app/src/main/java/allowance/fps/com/myallowance/FinanceManager.java
+++ app/src/main/java/allowance/fps/com/myallowance/FinanceManager.java
@@ -1,8 +1,11 @@
 package allowance.fps.com.myallowance;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import allowance.fps.com.myallowance.database.AllowanceDbHelper;
@@ -10,16 +13,21 @@ import allowance.fps.com.myallowance.model.Transaction;
 
 public class FinanceManager {
 
+  private static final String KEY_START_DATE = "PREF_START_DATE";
+
   private SavingManager mSavingManager;
 
   private AllowanceWallet mCurrentAllowance;
 
   private Calendar mStartDate;
 
+  final Context mAppContext;
+
   final private AllowanceDbHelper mDbHelper;
 
   public FinanceManager(Context context) {
-    mDbHelper = new AllowanceDbHelper(context.getApplicationContext());
+    mAppContext = context.getApplicationContext();
+    mDbHelper = new AllowanceDbHelper(mAppContext);
     mSavingManager = new SavingManager();
   }
 
@@ -28,20 +36,35 @@ public class FinanceManager {
   }
 
   public void setStartDate(Calendar startDate) {
+    SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(mAppContext);
+    sharedPreferences.edit().putLong(KEY_START_DATE, startDate.getTime().getTime()).apply();
     mStartDate = startDate;
+
     // when the start date changes, a new wallet is assigned
     mCurrentAllowance = mSavingManager.getNewAllowance(mCurrentAllowance);
   }
 
   public Calendar getStartDate() {
+    if (mStartDate == null) {
+      SharedPreferences sharedPreferences = PreferenceManager
+          .getDefaultSharedPreferences(mAppContext);
+
+      long startTime = sharedPreferences.getLong(KEY_START_DATE, -1);
+
+      if (startTime >= 0) {
+        Calendar startDate = Calendar.getInstance();
+        startDate.setTime(new Date(startTime));
+        mStartDate = startDate;
+      }
+    }
     return mStartDate;
   }
 
   public Calendar getEndDate() {
-    if (mStartDate == null) {
+    if (getStartDate() == null) {
       return null;
     }
-    Calendar endDate = (Calendar) mStartDate.clone();
+    Calendar endDate = (Calendar) getStartDate().clone();
     endDate.add(Calendar.DAY_OF_YEAR, 14);
     return endDate;
   }
