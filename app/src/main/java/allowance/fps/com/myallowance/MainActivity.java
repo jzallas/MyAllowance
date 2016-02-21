feee7c69dea3d5c14e161f373e4ea972fe18250d
Eileen Yau <eileen.yau@venmo.com>
2016-02-20 23:54:16 -0500

    update main activity

--- app/src/main/java/allowance/fps/com/myallowance/MainActivity.java
+++ app/src/main/java/allowance/fps/com/myallowance/MainActivity.java
@@ -11,12 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         getFragmentManager().beginTransaction()
                 .add(R.id.container, AccountReviewFragment.newInstance())
                 .commit();
