02ed76dc62169ba8149bc16ab55813815e2fe89a
Eileen Yau <eileen.yau@venmo.com>
2016-02-20 23:50:27 -0500

    Add stub account page

---
+++ app/src/main/java/allowance/fps/com/myallowance/AccountReviewFragment.java
@@ -0,0 +1,62 @@
+package allowance.fps.com.myallowance;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class AccountReviewFragment extends Fragment {
+
+
+    private TextView expectedSpendingText;
+
+    private TextView actualSpendingText;
+
+    private TextView budgetStart;
+
+    private TextView budgetEnd;
+
+    private FloatingActionButton addTransactionsFab;
+
+    public static AccountReviewFragment newInstance() {
+        return new AccountReviewFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootview = inflater.inflate(R.layout.fragment_account_review, container, false);
+        expectedSpendingText = (TextView) rootview
+                .findViewById(R.id.account_review_expended_spending_value);
+        actualSpendingText = (TextView) rootview
+                .findViewById(R.id.account_review_actual_spending_value);
+        budgetStart = (TextView) rootview.findViewById(R.id.account_review_start);
+        budgetEnd = (TextView) rootview.findViewById(R.id.account_review_goal);
+        addTransactionsFab = (FloatingActionButton) rootview.findViewById(R.id.add_transaction_fab);
+
+        initListener();
+        return rootview;
+    }
+
+    private void initListener() {
+        addTransactionsFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    private void initBudgetValue() {
+
+    }
+
+    private void initRecentTransactions() {
+
+    }
+}
--- app/src/main/java/allowance/fps/com/myallowance/MainActivity.java
+++ app/src/main/java/allowance/fps/com/myallowance/MainActivity.java
@@ -1,43 +1,46 @@
 package allowance.fps.com.myallowance;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import java.util.Calendar;
-
-import allowance.fps.com.myallowance.model.Transaction;
-
 public class MainActivity extends AppCompatActivity {
 
-  FinanceManager mFinanceManager = null;
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
-    return super.onOptionsItemSelected(item);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getFragmentManager().beginTransaction()
+                .add(R.id.container, AccountReviewFragment.newInstance())
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
